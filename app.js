6e264ab335abd46787887b5b068864c076737118
Kyu Young Han <khan67@uwo.ca>
2020-11-01 19:00:33 -0500

    Added placeholder for get

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require ('express');
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) =>{
     res.send('Hello World');
 }); 
 
-app.listen(3000, () => console.log ('Listening on port 3000...'));
+app.listen(port, ()=> {
+ console.log (`Listening on port ${port}`)
+});
